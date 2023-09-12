c88c464d00728a153210a155e8e8ad1b96b31699
rubel04 <alomrana886@gmail.com>
2023-09-12 12:22:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './COMPONENTS/Blogs/Blogs'
+import Header from './COMPONENTS/Header/Header'
+
+function App() {
+
+  return (
+    <>
+     <Header></Header>
+     <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
